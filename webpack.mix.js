e48ede175bc528ce10f646c76aaed557cfdae777
DavidLZisele <davidzisele2016@gmail.com>
2020-10-26 18:48:59 -0300

    listo sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/portfolio/js/app.js', 'public/portfolio/js/all.js')
+    .sass('resources/portfolio/scss/app.scss', 'public/portfolio/css/all.cs');
